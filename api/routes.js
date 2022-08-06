d0096cf750e9f207d3d1769b6b1366acfb4d67fa
VDimov01 <vdimov54@gmail.com>
2022-08-06 18:29:46 +0300

    update architecture

--- api/routes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const apiController = require('./controllers/apiController');
-
-router.use('/api', apiController);
-
-module.exports = router;
---
+++ server/index.js
@@ -0,0 +1,23 @@
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const express = require('express');
+const routes = require('./routes');
+const app = express();
+const { initDatabase } = require('./initDB.js');
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+
+app.use(routes);
+
+initDatabase()
+.then(() => {
+    app.listen(5000, () => console.log('App is listening on port 5000...'));
+    console.log('Database connected!');
+})
+.catch((err) => {
+    console.log('Cannot connect to DB!',err);
+})
