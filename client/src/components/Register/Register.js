cb0350e0beee73fa56a82cf4407e840c3fc54606
VDimov01 <vdimov54@gmail.com>
2022-08-07 13:31:05 +0300

    make the form controlled

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 import * as authService from "../../services/authService";
@@ -7,20 +7,37 @@ const Register = () => {
     
     const navigate = useNavigate();
     const {userLogin} = useContext(AuthContext);
+    const [user, setUser] = useState({
+        email:"",
+        password:"",
+        confirmPassword:""
+    });
+    const [errors, setErrors] = useState({});
+
+    const onChange = (e) => {
+        setUser(state => {
+            return {
+                ...state,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    const isMatch = (e) => {
+        setErrors(state => ({
+            ...state,
+            [e.target.name]: user.password !== user.confirmPassword,
+        }))
+    }
 
     const onSubmit = (e) => {
         e.preventDefault();
-        const {
-            email,
-            password,
-            confirmPassword
-        } = Object.fromEntries(new FormData(e.target));
 
-        if(password !== confirmPassword) {
+        if(user.password !== user.confirmPassword) {
             return;
         }
 
-        authService.register(email, password)
+        authService.register(user.email, user.password)
             .then(user => {
                 userLogin(user);
                 navigate('/');
@@ -32,16 +49,17 @@ const Register = () => {
         <form onSubmit={onSubmit}>
             <div className="form-group">
                 <label htmlFor="exampleInputEmail1">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" placeholder="Enter email" />
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" value={user.email} onChange={onChange} placeholder="Enter email" />
                 <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
             <div className="form-group">
                 <label htmlFor="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" name="password" placeholder="Password" />
+                <input type="password" className="form-control" id="exampleInputPassword1" name="password" value={user.password} onChange={onChange} placeholder="Password" />
             </div>
             <div className="form-group">
                 <label htmlFor="exampleInputPassword2">Confirm Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword2" name="confirmPassword" placeholder="Confirm Password" />
+                <input type="password" className="form-control" id="exampleInputPassword2" name="confirmPassword" value={user.confirmPassword} onBlur={(e) => isMatch(e)} onChange={onChange} placeholder="Confirm Password" />
+                {errors.confirmPassword && <p className="error">Passwords must match</p>}
             </div>
             <button type="submit" className="btn btn-primary">Submit</button>
         </form>
