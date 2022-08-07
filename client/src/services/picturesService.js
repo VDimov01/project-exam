54e115bc9824de2a6c995f46a66a313d47c291c8
VDimov01 <vdimov54@gmail.com>
2022-08-07 13:30:35 +0300

    Update picturesService.js

--- client/src/services/picturesService.js
+++ client/src/services/picturesService.js
@@ -6,3 +6,24 @@ export const getAll = () => {
         .then(res => res.json())
 }
 
+export const getOne = (id) => {
+    return fetch(`${baseUrl}/${id}`)
+        .then(res => res.json())
+}
+
+export const create = (picture) => {
+    return fetch(baseUrl, {
+        method: 'POST',
+        body: JSON.stringify(picture)
+    })
+        .then(res => res.json())
+}
+
+export const updateOne = (id, picture) => {
+    return fetch(`${baseUrl}/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify(picture)
+    })
+        .then(res => res.json())
+}
+
